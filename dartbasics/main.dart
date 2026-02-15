f792e877c29ab616dc955d42181b64dc5477b808
Max <slamax838@gmail.com>
2026-02-15 12:24:37 +0100

    operations2

--- dartbasics/main.dart
+++ dartbasics/main.dart
@@ -1,18 +1,11 @@
 main() {
-  String title = 'Dart Basics';
-  title = 'Dart Basics 2';
-
-  int stars = 5;
-  stars++;
-
   double raiting = 4.5;
-  raiting *= 2;
-
-  bool isGood = true;
-  isGood = false;
-
-  print(title);
-  print(stars);
-  print(raiting);
-  print(isGood);
+  var b = raiting % 4;
+  var c = raiting ~/ 4;
+  bool d = c == 1;
+  bool e = c != 1;
+  print(b);
+  print(c);
+  print(d);
+  print(e);
 }
