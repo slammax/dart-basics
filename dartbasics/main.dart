d04cf40ac397c67199851af8ee1b9926eebd22c1
Max <slamax838@gmail.com>
2026-02-15 12:09:06 +0100

    variables

--- dartbasics/main.dart
+++ dartbasics/main.dart
@@ -1,3 +1,16 @@
 main() {
-  print('Hello, World!');
+  String title = 'Dart Basics';
+  title = 'Dart Basics 2';
+
+  int stars = 5;
+
+  double raiting = 4.5;
+
+  bool isGood = true;
+  isGood = false;
+
+  print(title);
+  print(stars);
+  print(raiting);
+  print(isGood);
 }
