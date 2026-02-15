97facf91876e201d8c50b09990ec5eb21bb3be8d
Max <slamax838@gmail.com>
2026-02-15 12:47:58 +0100

    dynamic

--- dartbasics/main.dart
+++ dartbasics/main.dart
@@ -1,11 +1,6 @@
 main() {
-  const pi = 'Const variable cannot be changed';
-  final pi2 = 'Final variable cannot be changed too';
-
-  final date =
-      DateTime.now(); //Будет показывать дату в момент запуска программы
-
-  print(pi);
-  print(pi2);
-  print(date);
+  dynamic a = 10;
+  a = 'Hello';
+  a = true;
+  print(a); //Будет показывать последнее значение переменной a
 }
