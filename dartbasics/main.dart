bb42bdaad336fa27b1fdb2d860be555f82259c5b
Max <slamax838@gmail.com>
2026-02-15 20:07:47 +0100

    map (list)

--- dartbasics/main.dart
+++ dartbasics/main.dart
@@ -1,44 +1,20 @@
 void main() {
-  final students = <String>['Женя', 'Саша', 'Петя'];
-  print(students);
-  students.add('Мария');
-  print(students);
-  students.remove('Женя');
-  print(students);
-  students.removeAt(0);
-  print(students);
-
-  final firstStudent = students[1];
-  print(firstStudent);
-
-  students[1] = 'Настя';
+  final students = <String, double>{
+    'Макс': 4.5,
+    'Вера': 2,
+    'Саша': 3.4,
+    'Коля': 5,
+  };
   print(students);
 
-  students.addAll(['Виктор', 'Макс']);
+  students['Миша'] = 3.6;
   print(students);
 
-  print(students.contains('Виктор'));
+  students.addAll({'Женя': 2, 'Юра': 3.4});
+  print(students);
 
   print(students.length);
 
-  print(students.indexOf('Виктор'));
-
-  print(students.indexOf('Викт'));
-
-  final foundStudents = students.where((element) => true);
-  print(foundStudents);
-
-  final foundExactlyStudent = students
-      .where((element) => element == 'Макс')
-      .toList();
-  print(foundExactlyStudent);
-
-  students.clear();
-  print(students);
-
-  final students2 = <String>['Петя', 'Женя', 'Саша', 'Петя'];
-  print(students2);
-
-  final classes = <String>{'1a', '1b', '1a'}; // только уникальные значения
-  print(classes);
+  print(students.keys.toList());
+  print(students.values.toList());
 }
