66845e7203da51469f6110b450207fa9e7901129
Max <slamax838@gmail.com>
2026-02-16 12:54:52 +0100

    switch_case

---
+++ dartbasics/9.switch_case.dart
@@ -0,0 +1,17 @@
+void main() {
+  final student = 'Макс';
+
+  switch (student) {
+    case 'Макс':
+      print('Оценка 5.0');
+      break;
+    case 'Настя':
+      print('Оценка 4.5');
+      break;
+    case 'Саша':
+      print('Оценка 3.2');
+      break;
+    default:
+      print('Такого студента нет');
+  }
+}
