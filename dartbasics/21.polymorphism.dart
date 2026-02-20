f132847dac88f2d1c645872c47a79ad7884390e1
Max <slamax838@gmail.com>
2026-02-20 11:57:40 +0100

    polymorphism

---
+++ dartbasics/21.polymorphism.dart
@@ -0,0 +1,42 @@
+void main() {
+  final Animal wolf = Wolf(4);
+  final tiger = Tiger(5);
+
+  wolf.makeSound();
+  print(wolf.age);
+  tiger.makeSound();
+  tiger.makePerfoemanceInCircus();
+}
+
+abstract interface class Animal {
+  double get age;
+  void makeSound();
+}
+
+class Wolf implements Animal {
+  Wolf(this.age);
+
+  @override
+  void makeSound() {
+    print('Auuuu');
+  }
+
+  @override
+  final double age;
+}
+
+class Tiger implements Animal {
+  Tiger(this.age);
+
+  @override
+  void makeSound() {
+    print('Agrrr');
+  }
+
+  void makePerfoemanceInCircus() {
+    print('Выступает в цирке');
+  }
+
+  @override
+  final double age;
+}
