b54cf02a0756c13b2a4b98b523d0fc04b9140f04
Max <slamax838@gmail.com>
2026-02-22 15:22:25 +0100

    try to add convert

--- dartbasics/exchange/bin/exchange.dart
+++ dartbasics/exchange/bin/exchange.dart
@@ -1,10 +1,14 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 Future<void> main(List<String> arguments) async {
   final response = await http.get(
     Uri.parse(
-      'https://api.exchangerate.host/list?access_key=5e39fbfe2dbcb93fbb87a143a7eee73f',
+      'https://api.exchangerate.host/convert?from=EUR&to=GBP&amount=100&access_key=5e39fbfe2dbcb93fbb87a143a7eee73f',
     ),
   );
-  print(response.body);
+  final stringBody = response.body;
+  final data = JsonDecoder().convert(stringBody) as Map<String, dynamic>;
+  print(data['result']);
 }
