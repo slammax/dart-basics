d3cd6ed41d3a9bfb3b2b11ccec13805f732ac5e2
Max <slamax838@gmail.com>
2026-02-22 15:15:35 +0100

    connect API

--- dartbasics/exchange/bin/exchange.dart
+++ dartbasics/exchange/bin/exchange.dart
@@ -1,1 +1,10 @@
-void main(List<String> arguments) {}
+import 'package:http/http.dart' as http;
+
+Future<void> main(List<String> arguments) async {
+  final response = await http.get(
+    Uri.parse(
+      'https://api.exchangerate.host/list?access_key=5e39fbfe2dbcb93fbb87a143a7eee73f',
+    ),
+  );
+  print(response.body);
+}
