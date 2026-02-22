cd4b7c009210473f0e9516b22fedc2810a5fb375
Max <slamax838@gmail.com>
2026-02-22 14:12:52 +0100

    isolate args

--- dartbasics/34.isolate.dart
+++ dartbasics/34.isolate.dart
@@ -1,9 +1,19 @@
 import 'dart:isolate';
 
-void main() {
-  Isolate.spawn((String args) => sayHello(args), 'Макс');
+Future<void> main() async {
+  final receivePort = ReceivePort();
+  Isolate.spawn(mathOperation, [10, receivePort.sendPort]);
+
+  final result = await receivePort.first;
+  print(result);
 }
 
-void sayHello(String name) {
-  print('Привет $name из изолята');
+void mathOperation(List<Object> args) {
+  final number = args[0] as int;
+  final sendPort = args[1] as SendPort;
+
+  Future.delayed(Duration(seconds: 5)).whenComplete(() {
+    final result = number * number;
+    sendPort.send(result);
+  });
 }
