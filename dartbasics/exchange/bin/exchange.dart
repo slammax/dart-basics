7cac647803d96d9bc1826c96156856539e5c2b61
Max <slamax838@gmail.com>
2026-02-22 15:50:27 +0100

    end of exchange project

--- dartbasics/exchange/bin/exchange.dart
+++ dartbasics/exchange/bin/exchange.dart
@@ -3,11 +3,36 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<void> main(List<String> arguments) async {
+  if (arguments.length != 3) {
+    print('Нверный формат вызова');
+    return;
+  }
+
+  final fromCurrency = arguments[0];
+  final toCurrency = arguments[1];
+  final stringAmount = arguments[2];
+  if (double.tryParse(stringAmount) == null) {
+    print('Последним аргументом должно быть число');
+    return;
+  }
+  final amount = double.parse(stringAmount);
+
+  print('Начинаем подсчет обмена $fromCurrency $amount -> $toCurrency.');
+
   final response = await http.get(
-    Uri.parse(
-      'https://api.exchangerate.host/convert?from=EUR&to=GBP&amount=100&access_key=5e39fbfe2dbcb93fbb87a143a7eee73f',
+    Uri(
+      scheme: 'https',
+      host: 'api.exchangerate.host',
+      path: 'convert',
+      queryParameters: {
+        'from': fromCurrency,
+        'to': toCurrency,
+        'amount': amount.toString(),
+        'access_key': '5e39fbfe2dbcb93fbb87a143a7eee73f',
+      },
     ),
   );
+
   final stringBody = response.body;
   final data = JsonDecoder().convert(stringBody) as Map<String, dynamic>;
   print(data['result']);
